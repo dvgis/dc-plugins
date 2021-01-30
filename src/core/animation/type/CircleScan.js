ebb077de8ad3f157a7a950229d0ee4d71ed66a27
Caven Chen <cavencj@gmail.com>
2021-01-30 15:19:54 +0800

    重构圆形扩散后期效果

--- src/core/animation/type/CircleScan.js
+++ src/core/animation/type/CircleScan.js
@@ -7,18 +7,18 @@ import Animation from '../Animation'
 
 const CircleScanShader = require('../../shader/CircleScanShader.glsl')
 
-const { Transform, Position, Parse, Util } = DC
+const { Transform, Parse, Util } = DC
 
 const { Cesium } = DC.Namespace
 
 class CircleScan extends Animation {
   constructor(viewer, position, radius, options = {}) {
     super(viewer)
+    this._delegate = undefined
     this._position = Parse.parsePosition(position)
     this._radius = radius || 100
-    this._color = Cesium.defaultValue(options.color, Cesium.Color.RED)
-    this._duration = Cesium.defaultValue(options.duration, 1) * 1e3
-    this._delegate = undefined
+    this._color = options.color || Cesium.Color.RED
+    this._speed = options.speed || 2
     this.type = 'circle_scan'
   }
 
@@ -28,66 +28,29 @@ class CircleScan extends Animation {
    */
   _mountContent() {
     let cartesian3Center = Transform.transformWGS84ToCartesian(this._position)
-    let cartesian4Center = new Cesium.Cartesian4(
-      cartesian3Center.x,
-      cartesian3Center.y,
-      cartesian3Center.z,
-      1
-    )
-    let cartesian3Center1 = Transform.transformWGS84ToCartesian(
-      new Position(
-        this._position.lng,
-        this._position.lat,
-        this._position.alt + 500
-      )
-    )
-    let cartesian4Center1 = new Cesium.Cartesian4(
-      cartesian3Center1.x,
-      cartesian3Center1.y,
-      cartesian3Center1.z,
-      1
-    )
-    let _time = new Date().getTime()
-
+    let self = this
     this._delegate = new Cesium.PostProcessStage({
       name: Util.uuid(),
       fragmentShader: CircleScanShader,
       uniforms: {
-        u_scanCenterEC: () => {
-          return Cesium.Matrix4.multiplyByVector(
-            this._viewer.camera._viewMatrix,
-            cartesian4Center,
-            new Cesium.Cartesian4()
+        centerWC: function() {
+          return cartesian3Center
+        },
+        normalWC: function() {
+          return Cesium.Ellipsoid.WGS84.geodeticSurfaceNormal(
+            cartesian3Center,
+            new Cesium.Cartesian3()
           )
         },
-        u_scanPlaneNormalEC: () => {
-          let temp = Cesium.Matrix4.multiplyByVector(
-            this._viewer.camera._viewMatrix,
-            cartesian4Center,
-            new Cesium.Cartesian4()
-          )
-          let temp1 = Cesium.Matrix4.multiplyByVector(
-            this._viewer.camera._viewMatrix,
-            cartesian4Center1,
-            new Cesium.Cartesian4()
-          )
-          let _scratchCartesian3Normal = new Cesium.Cartesian3()
-          _scratchCartesian3Normal.x = temp1.x - temp.x
-          _scratchCartesian3Normal.y = temp1.y - temp.y
-          _scratchCartesian3Normal.z = temp1.z - temp.z
-          Cesium.Cartesian3.normalize(
-            _scratchCartesian3Normal,
-            _scratchCartesian3Normal
-          )
-          return _scratchCartesian3Normal
+        radius: function() {
+          return self._radius
         },
-        u_radius: () => {
-          return (
-            (this._radius * ((new Date().getTime() - _time) % this._duration)) /
-            this._duration
-          )
+        speed: function() {
+          return self._speed
         },
-        u_scanColor: this._color
+        color: function() {
+          return self._color
+        }
       }
     })
   }
--- src/core/shader/CircleScanShader.glsl
+++ src/core/shader/CircleScanShader.glsl
@@ -1,41 +1,46 @@
 uniform sampler2D colorTexture;
 uniform sampler2D depthTexture;
 varying vec2 v_textureCoordinates;
-uniform vec4 u_scanCenterEC;
-uniform vec3 u_scanPlaneNormalEC;
-uniform float u_radius;
-uniform vec4 u_scanColor;
+uniform vec3 centerWC;
+uniform vec3 normalWC;
+uniform float radius;
+uniform vec4 color;
+uniform float speed;
+
+float getDepth(){
+  float z_window = czm_unpackDepth(texture2D(depthTexture, v_textureCoordinates));
+  z_window = czm_reverseLogDepth(z_window);
+  float n_range = czm_depthRange.near;
+  float f_range = czm_depthRange.far;
+  return  (2.0 * z_window - n_range - f_range) / (f_range - n_range);
+}
 
 vec4 toEye(in vec2 uv, in float depth){
   vec2 xy = vec2((uv.x * 2.0 - 1.0),(uv.y * 2.0 - 1.0));
-  vec4 posInCamera = czm_inverseProjection * vec4(xy, depth, 1.0);
-  posInCamera =posInCamera / posInCamera.w;
+  vec4 posInCamera =czm_inverseProjection * vec4(xy, depth, 1.0);
+  posInCamera = posInCamera / posInCamera.w;
   return posInCamera;
 }
 
 vec3 pointProjectOnPlane(in vec3 planeNormal, in vec3 planeOrigin, in vec3 point){
-    vec3 v01 = point - planeOrigin;
-    float d = dot(planeNormal, v01) ;
-    return (point - planeNormal * d);
+  vec3 v01 = point - planeOrigin;
+  float d = dot(planeNormal, v01) ;
+  return (point - planeNormal * d);
 }
 
-float getDepth(in vec4 depth){
-    float z_window = czm_unpackDepth(depth);
-    z_window = czm_reverseLogDepth(z_window);
-    float n_range = czm_depthRange.near;
-    float f_range = czm_depthRange.far;
-    return (2.0 * z_window - n_range - f_range) / (f_range - n_range);
-}
-
-void main(){
-    gl_FragColor = texture2D(colorTexture, v_textureCoordinates);
-    float depth = getDepth(texture2D(depthTexture, v_textureCoordinates));
-    vec4 viewPos = toEye(v_textureCoordinates, depth);
-    vec3 prjOnPlane = pointProjectOnPlane(u_scanPlaneNormalEC.xyz, u_scanCenterEC.xyz, viewPos.xyz);
-    float dis = length(prjOnPlane.xyz - u_scanCenterEC.xyz);
-    if(dis < u_radius){
-      float f = 1.0 - abs(u_radius - dis) / u_radius;
-      f = pow(f, 4.0);
-      gl_FragColor = mix(gl_FragColor, u_scanColor, f);
-    }
+void main() {
+  gl_FragColor = texture2D(colorTexture, v_textureCoordinates);
+  float depth = getDepth();
+  vec4 viewPos = toEye(v_textureCoordinates, depth);
+  vec4 center = czm_view * vec4(centerWC,1);
+  vec4 planeNormal = czm_view * vec4(normalWC,0);
+  vec3 prjOnPlane = pointProjectOnPlane(planeNormal.xyz, center.xyz, viewPos.xyz);
+  float dis = length(prjOnPlane.xyz - center.xyz);
+  float time = fract(czm_frameNumber * speed / 1000.0);
+  float temp = radius * time;
+  if(dis < temp)  {
+    float f = 1.0 - abs(temp - dis) / temp;
+    f = pow(f, 4.0);
+    gl_FragColor = mix(gl_FragColor, color, f);
   }
+}
