21239939eadc1f399800a3e2d03db6a5a880732c
Caven Chen <cavencj@gmail.com>
2021-03-02 12:36:48 +0800

    添加圆形shader

--- src/core/material/shader/circle/CircleSpiralMaterial.glsl
+++ src/core/material/shader/circle/CircleSpiralMaterial.glsl
@@ -12,7 +12,7 @@ czm_material czm_getMaterial(czm_materialInput materialInput){
   czm_material material = czm_getDefaultMaterial(materialInput);
   vec2 st = materialInput.st * 2.0 - 1.0;
   st *= 1.6;
-  float time = fract(czm_frameNumber * speed / 1000.0);
+  float time = czm_frameNumber * speed / 1000.0;
   float r = length(st);
   float w = .3;
   st = rotate2D(st,(r*PI*6.-time*2.));
